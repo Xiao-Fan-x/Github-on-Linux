9ca75fb7b9d26dc3953f318e9f843e4cfaf500db
xiao <yz96848715@126.com>
2021-01-12 12:49:09 +0800

    add problem_4

--- LanQiao/src/2013_1/problem_4.java
+++ LanQiao/src/2013_1/problem_4.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class problem_4 {
 
   public static void main(String[] args) {
+    System.out.println(reverse("1958"));
     ArrayList<price> a1 = new ArrayList<price>();
     ArrayList<price> a2 = new ArrayList<price>();
 
@@ -20,15 +21,34 @@ public class problem_4 {
         a2.add(new price(i, plus));
     }
 
-    private static String reverse (String s){
-      char[] ans = new char[s.length()];
-      for (int i = s.length(); )
-
+    for (price p1:a1
+         ) {
+      for (price p2:a2
+           ) {
+        System.out.println(p1.pri+" "+p1.plus);
+        System.out.println(p2.pri+" "+p2.plus);
+      }
     }
+
   }
+  private static String reverse (String s){
+    char[] ans = new char[s.length()];
+    for (int i = s.length() - 1,  j = 0 ; i >= 0 ; i-- , j++){
+      char c = s.charAt(i);
+      if (c == '6') ans[j] = '9';
+      else if (c == '9') ans[j] = '6';
+      else ans[j] = c;
+    }
+    return new String(ans);
+  }
+
   private static class price {
     int pri;
     int plus;
 
+    public price(int pri, int plus) {
+      this.pri = pri;
+      this.plus = plus;
+    }
   }
 }
