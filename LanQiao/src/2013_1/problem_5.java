9de2dd57c333d3d0a065bc012392ace9310e0d48
Xiao-Fan-x <68093976+Xiao-Fan-x@users.noreply.github.com>
2021-01-18 15:35:41 +0800

    add win ac

--- LanQiao/src/2013_1/problem_5.java
+++ LanQiao/src/2013_1/problem_5.java
@@ -2,4 +2,30 @@ public class problem_5 {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+    static void sort(int[] x)
+    {
+        int p = 0;
+        int left = 0;
+        int right = x.length-1;
+
+        while(p<=right){
+            if(x[p]<0){
+                int t = x[left];
+                x[left] = x[p];
+                x[p] = t;
+                left++;
+                p++;
+            }
+            else if(x[p]>0){
+                int t = x[right];
+                x[right] = x[p];
+                x[p] = t;
+                right--;
+            }
+            else{
+                ________________p++________;  //代码填空位置
+            }
+        }
+    }
 }
