c3e8e609c7b6a2b697a8845e7229073b64276538
Xiao-Fan-x <68093976+Xiao-Fan-x@users.noreply.github.com>
2021-01-20 09:37:58 +0800

    aa

--- LanQiao/src/2013_1/problem_7.java
+++ LanQiao/src/2013_1/problem_7.java
@@ -1,4 +1,36 @@
-public class problem_7 {
+import jdk.internal.org.objectweb.asm.tree.LookupSwitchInsnNode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class problem_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        int N = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < N; i++) {
+            String line = sc.nextLine();
+            String[] split = line.split(" ");
+            for (int j = 0; j < split.length; j++){
+                list.add(Integer.parseInt(split[j]));
+            }
+        }
+//        System.out.println(list.size());
+        Collections.sort(list);
+        int a=0,b=0;
+
+        for (int i = 1; i < list.size(); i++){
+            if (list.get(i) - list.get(i-1)==2){
+                a = list.get(i) - 1;
+            }
+            if (list.get(i).equals( list.get(i-1))){
+                b = list.get(i);
+            }
+        }
+        System.out.println(a+" "+b);
+
+    }
 }
 
