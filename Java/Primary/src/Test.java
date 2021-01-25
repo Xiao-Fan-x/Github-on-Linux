ea103f32ab9c2f4fb541c1efa1c1a5f7f1261f10
Xiao-Fan-x <68093976+Xiao-Fan-x@users.noreply.github.com>
2021-01-25 10:44:57 +0800

    a

--- Java/Primary/src/Test.java
+++ Java/Primary/src/Test.java
@@ -1,13 +1,31 @@
 import edu.princeton.cs.algs4.StdDraw;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println(binomial(100,50,0.25));
+
+    {
+        System.out.println("普通代码块");
     }
 
-    public static double binomial(int N,int k,double p){
-        if (N == 0 && k == 0)   return 1.0;
-        if (N < 0 || k < 0)     return 0.0;
-        return (1.0-p)*binomial(N-1,k,p) + p*binomial(N-1,k-1,p);
+    public Test(){
+
+        {
+            System.out.println("构造块");
+        }
+
+        System.out.println("构造方法");
     }
+
+    {
+        System.out.println("普通代码块2");
+    }
+
+    public static void main(String[] args) {
+        new Test();
+        new Test();
+        new Test();
+
+    }
+
+
+
 }
