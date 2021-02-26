c05141f9af9b9b17164a55cf89d35f10a25ac31f
Xiao-Fan-x <68093976+Xiao-Fan-x@users.noreply.github.com>
2021-02-26 16:12:50 +0800

    add

---
+++ LanQiaoStudy/Zhengshu.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Zhengshu {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        fun(n);
+    }
+
+    public static void fun(int n){
+        if (n == 0){
+            return;
+        }
+        for (int i=1;i<=n;i++){
+
+
+
+
+
+        }
+    }
+
+}
