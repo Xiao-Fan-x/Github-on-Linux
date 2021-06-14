f5e543f84fda6149d46b728ac35367d5cf986727
Xiao-Fan-x <yz96848715@126.com>
2021-06-14 13:57:38 +0800

    修改：     .idea/misc.xml 	修改：     Java/Primary/src/Test.java 	修改：     Java/Primary/src/Whitelist.java 	删除：     out/production/Github-on-Linux/Member.class 	修改：     out/production/Github-on-Linux/Test.class 	修改：     out/production/Github-on-Linux/Whitelist.class 	Java/Primary/src/CompareVersionDemo.java 	out/production/Github-on-Linux/CompareVersionDemo.class

--- Java/Primary/src/Test.java
+++ Java/Primary/src/Test.java
@@ -1,32 +1,79 @@
+import java.text.ParseException;
+import java.util.Scanner;
+
 public class Test {
-    public static void main(String[] args) throws CloneNotSupportedException {
-        Member memberA = new Member("xiaofan",16);
-        Member memberB = (Member) memberA.clone();
-        System.out.println(memberA.toString());
-        System.out.println(memberB.toString());
+    public static void main(String[] args) throws CloneNotSupportedException, ParseException {
+        Scanner input = new Scanner(System.in);
+        String v1 = input.next();
+        String v2 = input.next();
+
+        int res = compareVersion(v1, v2);
+        System.out.println(res);
     }
+
+    private static int compareVersion(String v1, String v2) {
+        int turned = 1;
+        int isZero = 1;
+        int v1_length = v1.split("\\.").length;
+        int v2_length = v2.split("\\.").length;
+
+        if (v1_length < v2_length) {
+            String temp = v1;
+            v1 = v2;
+            v2 = temp;
+            turned = -1;
+        }
+
+        while (v2.split("\\.").length >= 2) {
+            String v1_str[] = v1.split("\\.", 2);
+            String v2_str[] = v2.split("\\.", 2);
+
+            int v1num = Integer.valueOf(v1_str[0]);
+            int v2num = Integer.valueOf(v2_str[0]);
+
+            if (v1num > v2num) {
+                return 1;
+            } else if (v1num < v2num) {
+                return -1;
+            }
+
+            v1 = v1_str[1];
+            v2 = v2_str[1];
+        }
+
+        String v1_str[] = v1.split("\\.", 2);
+        if (v1_str[1] != null) {
+            v1 = v1_str[1];
+            char[] v1_str1 = v1.toCharArray();
+            for (int i = 0; i < v1.length(); i++) {
+                if (v1_str1[i] > '0' && v1_str1[i] < '9') isZero = -1;
+            }
+
+            if (isZero == 1) {
+                int v1num = Integer.valueOf(v1_str[0]);
+                int v2num = Integer.valueOf(v2);
+
+                if (v1num > v2num) {
+                    return 1 * turned;
+                } else if (v1num < v2num) {
+                    return -1 * turned;
+                } else {
+                    return 0;
+                }
+            }
+        }
+
+        int v1num = Integer.valueOf(v1);
+        int v2num = Integer.valueOf(v2);
+
+        if (v1num > v2num) {
+            return 1 * turned;
+        } else if (v1num < v2num) {
+            return -1 * turned;
+        }else return 0;
+    }
+
 }
 
-class Member implements Cloneable{
-    private String name;
-    private int age;
 
-    public Member(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 
-    @Override
-    public String toString() {
-        return super.toString()+
-                " Member{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-}
--- Java/Primary/src/Whitelist.java
+++ Java/Primary/src/Whitelist.java
@@ -5,14 +5,22 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Whitelist {
     public static void main(String[] args) {
-        int[] w= In.readInts(args[0]);
-        StaticSETofInts set = new StaticSETofInts(w);
-        while (!StdIn.isEmpty()) {
-            //读取键，如果不再白名单中则打印它
-            int key = StdIn.readInt();
-            if (!set.contains(key)) {
-                StdOut.println(key);
-            }
-        }
+//        int[] w= In.readInts(args[0]);
+//        StaticSETofInts set = new StaticSETofInts(w);
+//        while (!StdIn.isEmpty()) {
+//            //读取键，如果不再白名单中则打印它
+//            int key = StdIn.readInt();
+//            if (!set.contains(key)) {
+//                StdOut.println(key);
+//            }
+//        }
+        String str = "0002";
+        int num = Integer.parseInt(str);
+        System.out.println(num);
+        String str1 = "1231.2";
+        System.out.println(str.split("\\.",2)[0]);
+        int num1 = Integer.parseInt(str.split("\\.",2)[0]);
+        System.out.println(num);
+        System.out.println(str.split("\\.").length);
     }
 }
