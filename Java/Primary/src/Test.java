cbad0951720ac390b81df27ca19d9c62667c2f6f
Xiao-Fan-x <yz96848715@126.com>
2021-06-25 21:29:10 +0800

    modified:   Java/Primary/src/Test.java 	modified:   out/production/Github-on-Linux/Test.class 	modified:   out/production/Github-on-Linux/Main.class 	out/production/Github-on-Linux/Solution1.class

--- Java/Primary/src/Test.java
+++ Java/Primary/src/Test.java
@@ -1,15 +1,9 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Locale;
 
 public class Test {
     public static void main(String[] args) {
-        String str = "iaodjsadjosa#{dasd}()dsdad";
-        String regex = "#\\{\\w+\\}";
-        Pattern pat = Pattern.compile(regex);
-        Matcher mat = pat.matcher(str);
-        while (mat.find()){
-            System.out.println(mat.group(0).replaceAll("#|\\{|\\}",""));
-        }
+        Locale loc = new Locale("zh","CN");
+        System.out.println(loc);
     }
 }
 
