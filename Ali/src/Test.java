5e6664226552c7b9ad6b9f0858226cdad0e79bdd
Xiao-Fan-x <68093976+Xiao-Fan-x@users.noreply.github.com>
2021-08-09 15:46:33 +0800

    a

--- Ali/src/Test.java
+++ Ali/src/Test.java
@@ -1,19 +1,105 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class Test {
-    public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Date resultDate = new Date();
-        String starttime = sdf.format(resultDate);
-        Calendar ca = Calendar.getInstance();
-        ca.add(Calendar.YEAR, +1); // 年份减1
-        resultDate = ca.getTime(); // 结果
-        String endtime = sdf.format(resultDate);
-        System.out.println(starttime+"  "+endtime);
+    public static void main(String[] args) throws IOException {
+//        String str = "www.baidu.com";
+//        InputStream input = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
+//        ByteArrayOutputStream output = new ByteArrayOutputStream();
+//
+//        int data = 0;
+//        while ((data = input.read()) != -1){
+//            output.write(Character.toUpperCase((char) data));
+//        }
+//        byte result[] = output.toByteArray();
+//        System.out.println(output);
+//        System.out.println(new String(result));
+//
+//        output.close();
+//        input.close();
 
-//         System.out.println(sdf.format(date));
+
+        URL url = new URL("http://www.bing.com");
+
+        /* 字节流 */
+        InputStream is = url.openStream();
+
+        /* 字符流 */
+        InputStreamReader isr = new InputStreamReader(is, "utf-8");
+
+        /* 提供缓存功能 */
+        BufferedReader br = new BufferedReader(isr);
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        br.close();
+
     }
 }
+
+/**
+ * @description 将普通变量升级为原子变量
+ **/
+//class AtomicIntegerFieldUpdaterTest implements Runnable {
+//
+//    static Goods phone;
+//    static Goods computer;
+//
+//    AtomicIntegerFieldUpdater<Goods> atomicIntegerFieldUpdater =
+//            AtomicIntegerFieldUpdater.newUpdater(Goods.class, "price");
+//
+//    @Override
+//    public void run() {
+//        for (int i = 0; i < 10000; i++) {
+//            phone.price++;
+//            atomicIntegerFieldUpdater.getAndIncrement(computer);
+//        }
+//    }
+//
+//    static class Goods {
+//        //商品定价
+//        volatile int price;
+//    }
+//
+//    public static void main(String[] args) throws InterruptedException {
+//        phone = new Goods();
+//        computer = new Goods();
+//        AtomicIntegerFieldUpdaterTest atomicIntegerFieldUpdaterTest = new AtomicIntegerFieldUpdaterTest();
+//        Thread thread1 = new Thread(atomicIntegerFieldUpdaterTest);
+//        Thread thread2 = new Thread(atomicIntegerFieldUpdaterTest);
+//        thread1.start();
+//        thread2.start();
+//        //join()方法是为了让main主线程等待thread1、thread2两个子线程执行完毕
+//        thread1.join();
+//        thread2.join();
+//        System.out.println("CommonInteger price = " + phone.price);
+//        System.out.println("AtomicInteger price = " + computer.price);
+//    }
+//}
+//
+
+//class CountdownLatchExample {
+//
+//    public static void main(String[] args) throws InterruptedException {
+//        final int totalThread = 10;
+//        CountDownLatch countDownLatch = new CountDownLatch(totalThread);
+//        ExecutorService executorService = Executors.newCachedThreadPool();
+//        for (int i = 0; i < totalThread; i++) {
+//            executorService.execute(() -> {
+//                System.out.print("run..");
+//                countDownLatch.countDown();
+//            });
+//        }
+//        countDownLatch.await();
+//        System.out.println("end");
+//        executorService.shutdown();
+//    }
+//}
